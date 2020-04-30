1713ee3550f0a0db2c9bf6d5fa08e8502bfb4c02
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 21:31:04 +0800

    Now let us try make directory, as we mentioned before, async way needs callback, sync way does not need.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 // Read and Write files (fs)
 var fs = require('fs');
 
-// Or use synchronous is ok
-// Now you deleted writeMe.txt, if you execute app.js again, there'll be error
-fs.unlinkSync('writeMe.txt');
+fs.mkdirSync('stuff');

1346fec8b8bd54ead3f03054b0e885a488260fe7
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 21:20:24 +0800

    We can delete file by unlink function.

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 // Read and Write files (fs)
 var fs = require('fs');
 
-// Asynchronous way needs the third argument, which is a callback function, the callback takes two arguments: error and data.
-var readMe = fs.readFile('readMe.txt', 'utf-8', function(err, data) {
-	console.log(data);
-});
-fs.writeFile('writeMe.txt', readMe, (err) => {
-	if (err) {
-		throw err;
-	}
-	console.log('write file successfully');
-});
+fs.unlink('writeMe.txt');

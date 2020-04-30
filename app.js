283076436401bfc4b0d0349a80d0610715239fb2
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 20:52:04 +0800

    Now let us try asynchronous way.

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
 // Read and Write files (fs)
 var fs = require('fs');
-
-// This function is synchronous, which will hold the program before finishing executing
-var readMe = fs.readFileSync('readMe.txt', 'utf-8');
-console.log(readMe);
-
-var arr = [ 1, 2, 3 ];
-
-// This function will write file synchronously, the second argument is for data.
-fs.writeFileSync('writeMe.txt', arr);

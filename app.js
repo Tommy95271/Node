23fdb5477dfded417e0e45512cfe50193789c767
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 19:58:14 +0800

    Get built-in module events.

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-// Then reuqire everything from the file
-var stuff = require('./stuff');
-
-// Now you can use the functions inside other files
-console.log(stuff.counter([ 'go', 'og', 'gdo' ]));
-console.log(stuff.adder(3, 6));
-console.log(stuff.adder(stuff.pi, 3));
+// event emit
+var events = require('events');

ed273d942acf5683566e26c7b941158eac36b858
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 22:03:29 +0800

    The file and directory are still there, why?

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ var fs = require('fs');
 // Now let's remove file and directory.
 // We can't remove directory if it is not empty
 // So let's delete the file inside stuff first
+fs.unlink('writeMe.txt', () => {
+	console.log('Remove the file successfully!');
+	fs.rmdir('stuff', () => {
+		console.log('Remove the directory successfully!');
+	});
+});

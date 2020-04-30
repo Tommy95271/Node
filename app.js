edc8c404d086b1edec74e864717590135b18b0e2
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 20:11:01 +0800

    Let us do something cool.

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
 // event emit
 var events = require('events');
-
-// create new event
-var myEmitter = new events.EventEmitter();
-
-// similar to jQuery, but you can define your own event instead of using click or keyup etc.
-myEmitter.on('event', function(msg) {
-	console.log(msg);
-});
-
-// emit the emitter with your event and the msg
-myEmitter.emit('event', 'this is a new file');

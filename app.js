aaf28ccdc2c4e8971ed83a029e4ed74e3b30f749
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 20:17:10 +0800

    Create three Person object.

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var events = require('events');
 // get util module so we can inherit
 var util = require('util');
 
+// Create a Person object
 var Person = function(name) {
 	this.name = name;
 };
 
+// Let Person inherit from events.EventEmitter
 util.inherits(Person, events.EventEmitter);
+
+// Create three Person instance
+var tommy = new Person('tommy');
+var mary = new Person('mary');
+var laker = new Person('laker');

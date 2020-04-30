2dcdbf95dbe265863316201871a860de7b576a43
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 20:48:33 +0800

    Now let us write file, as you can guess, writeFileSync, first argument is for the file you want to write, second is for the data you want to write in.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Read and Write files (fs)
 var fs = require('fs');
 
-// This function is Synchronous, which will hold the program before finishing executing
+// This function is synchronous, which will hold the program before finishing executing
 var readMe = fs.readFileSync('readMe.txt', 'utf-8');
 console.log(readMe);
+
+// This function will write file synchronously, the second argument is for data.
+fs.writeFileSync('writeMe.txt', readMe);

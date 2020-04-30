ff7c506baa630d7464d7ec689a46d6ae86d6727c
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 21:24:28 +0800

    Or use unlinkSync can delete file without callback.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Read and Write files (fs)
 var fs = require('fs');
 
-// delete file by unlink, but it is asynchronous, so it needs callback function
-fs.unlink('writeMe.txt');
+// Or use synchronous is ok
+fs.unlinkSync('writeMe.txt');

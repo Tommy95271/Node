a1087b9c72f6c415f755a2282caea5b5d64907ba
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 21:12:57 +0800

    We can separate readFile and writeFile, it just depends on what do you want to do.

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 
 // Asynchronous way needs the third argument, which is a callback function, the callback takes two arguments: error and data.
 var readMe = fs.readFile('readMe.txt', 'utf-8', function(err, data) {
-	fs.writeFile('writeMe.txt', data, (err) => {
-		if (err) {
-			throw err;
-		}
-		console.log('write file successfully');
-	});
+	console.log(data);
+});
+fs.writeFile('writeMe.txt', readMe, (err) => {
+	if (err) {
+		throw err;
+	}
+	console.log('write file successfully');
 });

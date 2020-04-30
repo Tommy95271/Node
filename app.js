4f26b74292695eeb08a70dd5abfce04fb89d9b94
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 22:04:20 +0800

    Because we forgot the relative path.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 // Now let's remove file and directory.
 // We can't remove directory if it is not empty
 // So let's delete the file inside stuff first
-fs.unlink('writeMe.txt', () => {
+// The relative path is wrong!
+fs.unlink('./stuff/writeMe.txt', () => {
 	console.log('Remove the file successfully!');
 	fs.rmdir('stuff', () => {
 		console.log('Remove the directory successfully!');

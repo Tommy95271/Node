c79dcbea2205f72de73053f364b2ea93ae32afcd
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 21:49:56 +0800

    It is a little bit complicated, we make a directory first, then read the data from readMe.txt, then write the data into writeMe.txt.

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // Read and Write files (fs)
 var fs = require('fs');
 
-fs.rmdirSync('stuff');
+// Let's add a directory and a file
+// First, make a directoy, remember async way needs callback.
+fs.mkdir('stuff', () => {
+	// Then read the file readMe.txt and get the data by callback
+	fs.readFile('readMe.txt', 'utf8', (err, data) => {
+		// Third, write the data into writeMe.txt, but we want it inside stuff directory, then call callback.
+		fs.writeFile('./stuff/writeMe.txt', data, (err) => {
+			if (err) throw err;
+		});
+	});
+});

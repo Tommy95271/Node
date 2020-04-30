95417b1bee1ce731d81ca4daaa0845e3a7bd3d44
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 15:16:17 +0800

    The way to export files and require from it.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-var counter = require('./counter');
+// Then reuqire everything from the file
+var counter = require('./count');
 
+// Now you can use the functions inside other files
 console.log(counter([ 'go', 'og', 'gdo' ]));
--- counter.js
+++
@@ -1,4 +0,0 @@
-var counter = function(arr) {
-	return `there are ${arr.length} elements in this array`;
-};
-module.exports = counter;

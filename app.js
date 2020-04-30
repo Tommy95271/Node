0863ed43f8f3def4efd330efc98893638570f8cf
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 21:54:57 +0800

    Remember, whatever you want to make or delete directory, read or write file, you need to get the right path, relative path usually. We did not write path but only file name before is because the files are in same directory, so we did not need relative path. But we are making file inside another directory now, relative paht is needed.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Read and Write files (fs)
 var fs = require('fs');
 
+// Remember, whatever you want to make or delete directory, read or write file, you need to get the right path, relative path usually.
+// We didn't write path but only file name before is because the files are in same directory, so we didn't need relative path.
+// But we are making file inside another directory now, relative paht is needed.
 // Let's add a directory and a file
 // First, make a directoy, remember async way needs callback.
 fs.mkdir('stuff', () => {

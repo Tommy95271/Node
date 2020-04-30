ac3a51d41612cf56ddd1baa10965965cf4aa1d30
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 15:26:13 +0800

    Changed count.js to sutff.js, because we need multiple functions or varialbes inside it.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Then reuqire everything from the file
-var counter = require('./count');
+var counter = require('./stuff');
 
 // Now you can use the functions inside other files
 console.log(counter([ 'go', 'og', 'gdo' ]));

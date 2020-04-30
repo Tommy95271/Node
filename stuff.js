d739f4090c876f91bc3aa30c1ae8b93f5db725b4
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 15:32:33 +0800

    The first way to export multiple functions and variables.

--- stuff.js
+++ stuff.js
@@ -1,14 +1,13 @@
-// There are multiple stuff, how can we export everything?
-var counter = function(arr) {
+// Or we can do this way, because anyway we need to export them, don't we?
+module.exports.counter = function(arr) {
 	return `there are ${arr.length} elements in this array`;
 };
 
-var adder = function(a, b) {
+module.exports.adder = function(a, b) {
 	return a + b;
 };
-var pi = 3.14;
+module.exports.pi = 3.14;
 
-// Doing this way is ok, but troublesome
-module.exports.counter = counter;
-module.exports.adder = adder;
-module.exports.pi = pi;
+// module.exports.counter = counter;
+// module.exports.adder = adder;
+// module.exports.pi = pi;

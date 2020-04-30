0d17f00f27c5dc3b7fd7b96e97d7294ed691d698
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 20:46:02 +0800

    readFileSync is synchronous, which is not good if you got multiple program.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Read and Write files (fs)
 var fs = require('fs');
 
+// This function is Synchronous, which will hold the program before finishing executing
 var readMe = fs.readFileSync('readMe.txt', 'utf-8');
 console.log(readMe);

8ad5f533511f74fe01578d30a6fa061ca236e9bb
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-30 20:02:28 +0800

    It is similar to jQuery, but you can define your own event.

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ var events = require('events');
 
 // create new event
 var myEmitter = new events.EventEmitter();
+
+// similar to jQuery, but you can define your own event instead of using click or keyup etc.
+myEmitter.on('event', function(msg) {
+	console.log(msg);
+});

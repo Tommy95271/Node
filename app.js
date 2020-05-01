dda60ddde30af11d3bb4c1a42a5bedcf55ec95d2
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-01 16:09:20 +0800

    Looks like it needs lots of time of fix it, but I need to focus on js, so leave it here.

--- app.js
+++
@@ -1,7 +0,0 @@
-// Then reuqire everything from the file
-// var stuff = require('./stuff');
-import * as stuff from 'stuff';
-// Now you can use the functions inside other files
-console.log(stuff.counter([ 'go', 'og', 'gdo' ]));
-console.log(stuff.adder(3, 6));
-console.log(stuff.adder(stuff.pi, 3));
